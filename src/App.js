3cb8f069ae49b806920addf71f510d53d83ecb1d
AlaaNaser95 <alaanaser95.95@gmail.com>
2018-10-31 16:35:00 +0300

    task submission

--- src/App.js
+++ src/App.js
@@ -8,13 +8,57 @@ const styles = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "black",
+      italic: false,
+      underline: false,
+      bold: false
+    };
+    this.changeColor = this.changeColor.bind(this);
+  }
+  changeColor(color) {
+    this.setState({ color: color });
+  }
+  italic() {
+    if (this.state.italic === false) {
+      this.setState({ italic: "italic" });
+    } else {
+      this.setState({ italic: false });
+    }
+  }
+  bold() {
+    if (this.state.bold) {
+      this.setState({ bold: false });
+    } else {
+      this.setState({ bold: "bold" });
+    }
+  }
+  underline() {
+    if (this.state.underline) {
+      this.setState({ underline: false });
+    } else {
+      this.setState({ underline: "underline" });
+    }
+  }
+  shaded(style) {
+    if (this.state[style]) {
+      return "blue";
+    }
+    return "";
+  }
   render() {
     let stylings = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = stylings.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          onClick={() => this[style]()}
+          style={{ backgroundColor: this.shaded(style) }}
+          key={style}
+        >
           {style}
         </button>
       );
@@ -23,6 +67,7 @@ class App extends Component {
     let colorBoxes = colors.map(color => {
       return (
         <button
+          onClick={() => this.changeColor(color)}
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
         />
@@ -35,7 +80,14 @@ class App extends Component {
         {stylingBoxes}
         <br />
         <br />
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            ...styles[this.state.bold],
+            ...styles[this.state.italic],
+            ...styles[this.state.underline]
+          }}
+        />
         <br />
         {colorBoxes}
       </div>
